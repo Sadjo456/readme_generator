6da2c70f967c50e3f758151d7d985a05cd131841
hassimioudiallo <hddinguiraye@gmail.com>
2023-03-24 15:51:09 -0400

    add rest of data

--- index.js
+++ index.js
@@ -1,13 +1,89 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+// An array of inquirer question object
+//inquirer
+    //.prompt
+   var questions = ([
+        {
+            type: 'input',
+            name:'projectName',
+            message: 'What is the name of your project?',    
+        },
+        {
+            type: 'input',
+            name:'description',
+            message: 'Provide a brief description of your project.',    
+        },
+        {
+            type: 'input',
+            name:'usage',
+            message: 'Provide instructions and examples of how to use your project.',    
+        },
+        {
+            type: 'input',
+            name:'username',
+            message: 'What is your GitHub username?',   
+        },
+        {
+            type: 'input',
+            name:'instalation',
+            message: 'Provide the steps needed to install your project.',    
+        },
+        {
+            type: 'input',
+            name:'tests',
+            message: 'Provide any tests for your application.',    
+        },
+        {
+            type: 'input',
+            name:'license',
+            message: 'Choose a license for your project.'
+        },       
+        {
+            type: 'list',
+            name:'contact',
+            message: 'What is your preferred method of communication?',
+            choices: ['email', 'phone', 'telekinesis'],    
+        },
+])
+//.then((data) => {
+   // const filename = `${data.name.toLowerCase().split(' ').join(' ')}.json`);
+//}
+//})
 
+
+
+    
+    
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile (fileName, data) {
+
+
+   return fs.writeFile(path.join(process.cwd(),fileName),data)
+      
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+//A funtion to take the data from the inquirer and generate a formated readme and recommended to use template literal.
+  
+    
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        writeToFile("README.md",generateMarkown({ ...answers}))
+    })
+
+    
+         
+   }
+    // inquirer.prompt
+    // .then => first call the function to format the readme
+    // call the write to file function
 
 // Function call to initialize app
 init();
+// Know requirement from README to create question for line 5
+// Use activity 20 and project 28 for guidance and how create question
+// Also for line 8 as well write to file function
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,117 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `![github license](https://img.shields.io/badge/license-${license}-green.svg)
+    `;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return `[License](#license)
+    `;
+  }
+  return "";
+}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return `## License`;
+  }
+  return "";
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+
+  ${data.description}
+
+  
+  
+  ## Table of Contents (Optional)
+  
+  -[Project Name](#projectName)
+  -[Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#Contributing)
+  - [Tests](#Tests)
+  - [User Name](#GitHub)
+  - [E-mail](#E-mail)
+  - ${renderLicenseLink(data.license)}
+  
+  ## Project Name
+
+  ${data.projectName}
+
+  ## Description
+
+  ${data.description}
+  
+  ## Installation
+
+  ${data.instalation}
+  
+  
+  ## Usage
+
+  ${data.usage}
+
+  ## User Name
+
+  ${data.github}
+
+  ## Contact
+
+  ${data.contact}
+
+  
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  ${data.tests}
+
+
+  
+  ${renderLicenseSection(data.license)}
+  
+  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+  
+  ---
+  
+  🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+  
+  ## Badges
+  
+  ${renderLicenseBadge(data.license)}
+  
+  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+  
+  ## Features
+  
+  If your project has a lot of features, list them here.
+  
+  ## How to Contribute
+  
+  If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+  
+  ## Tests
 `;
 }
 
